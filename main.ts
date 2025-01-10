7447ddbf14cceb83b924f644a4b67e5af8f8528b
wakywayne80@gmail.com <wakywayne80@gmail.com>
2025-01-09 20:22:05 -0500

    Plugin currently works

--- main.ts
+++ main.ts
@@ -1,134 +1,93 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, MarkdownView, Plugin, TFile, Notice } from "obsidian";
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface Hotstring {
+	trigger: string;
+	replacement: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class HotstringsPlugin extends Plugin {
+	private hotstrings: Hotstring[] = [];
+	private hotstringsFile: string = "hotstrings.md"; // File name for hotstrings
 
 	async onload() {
-		await this.loadSettings();
+		// Load hotstrings on startup
+		await this.loadHotstrings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
+		// Add a command to reload hotstrings
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
+			id: "reload-hotstrings",
+			name: "Reload Hotstrings",
+			callback: async () => {
+				await this.loadHotstrings();
+				new Notice("Hotstrings reloaded!");
+			},
+		});
+
+		// Listen for editor changes to apply hotstrings
+		this.registerEvent(
+			this.app.workspace.on("editor-change", (editor: Editor) => {
+				this.handleEditorChange(editor);
+			}),
+		);
+	}
+
+	// Load hotstrings from the user's `hotstrings` file
+	private async loadHotstrings() {
+		this.hotstrings = []; // Reset current hotstrings
+		const file = this.app.vault.getAbstractFileByPath(this.hotstringsFile);
+
+		if (file instanceof TFile) {
+			// If the file exists, read and parse it
+			const content = await this.app.vault.read(file);
+			this.parseHotstrings(content);
+		} else {
+			// Create the file with a default comment if it doesn't exist
+			await this.app.vault.create(
+				this.hotstringsFile,
+				`# Add your hotstrings below in the format:\n# trigger~.~{replacement}`,
+			);
+			new Notice(`Hotstrings file created: ${this.hotstringsFile}`);
+		}
+	}
+
+	// Parse the content of the `hotstrings` file
+	private parseHotstrings(content: string) {
+		const lines = content.split("\n");
+		lines.forEach((line) => {
+			// Ignore comments or empty lines
+			if (!line.trim() || line.startsWith("#")) return;
+
+			// Match the trigger~.~{replacement} format
+			const match = line.match(/^(.+?)~\.~\{(.+?)\}$/);
+			if (match) {
+				const [, trigger, replacement] = match;
+				this.hotstrings.push({ trigger, replacement });
 			}
 		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
+	}
+
+	// Apply hotstrings in the editor when typing
+	private handleEditorChange(editor: Editor) {
+		const cursor = editor.getCursor();
+		const line = editor.getLine(cursor.line);
+
+		this.hotstrings.forEach(({ trigger, replacement }) => {
+			// Check if the line ends with a trigger
+			if (line.endsWith(trigger)) {
+				// Replace the trigger with the replacement text
+				const newLine = line.replace(
+					new RegExp(`${trigger}$`),
+					replacement,
+				);
+				editor.setLine(cursor.line, newLine);
+
+				// Move the cursor to the end of the replacement text
+				editor.setCursor({ line: cursor.line, ch: newLine.length });
 			}
 		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+		console.log("HotstringsPlugin unloaded.");
 	}
 }
